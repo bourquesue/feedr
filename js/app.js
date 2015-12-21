fb67c7138aee0f43b6c3cf136886fcf54561062d
Sue Bourque <susanebourque@gmail.com>
2015-12-21 14:46:53 -0500

    Added reddit api

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
 /*
   Please add all Javascript code to this file.
 */
+ 
+(function(){
+	var httpRequest = new XMLHttpRequest();
+
+	document.getElementById('getReddit').onclick = request;
+
+	function request (){
+		httpRequest.onreadystatechange = responseMethod;
+
+		httpRequest.open('GET', 'https://www.reddit.com/top.json');
+
+		httpRequest.send();	
+	}
+
+	function responseMethod(){
+	  if(httpRequest.readyState === XMLHttpRequest.DONE)
+		if(httpRequest.status === 200){
+			console.log(httpRequest.responseText);
+		} else{
+			console.log("There was an error.")
+		}
+	}
+
+})();
